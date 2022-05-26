51eac26ad2b98ea04114f7c43687a755dfe9f449
Aniket Dattatraya Kulkarni <anikerry@gmail.com>
2022-05-26 18:26:15 +0530

    Add files via upload

---
+++ src/main/java/com/prog/EmpManagementAniketApplication.java
@@ -0,0 +1,13 @@
+package com.prog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpManagementAniketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpManagementAniketApplication.class, args);
+	}
+
+}
